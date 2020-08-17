54940de79c42e4b96b9949205405c61146e51cd5
owatkins <oisin.watkins@analog.com>
2020-08-17 09:49:59 +0100

    Reorganised the Repo, now only the essential files are present

--- MATLAB_Scripts/Wnp.m
+++
@@ -1,5 +0,0 @@
-function [twiddle] = Wnp(N,p)
-%Wnp Generates a single twiddle factor given input arguments N and p
-twiddle = exp(-1i * (2 * p * pi)/N);
-end
-
--- Neural_Net_Theano.py
+++
@@ -1,5 +0,0 @@
-import theano
-import theano.tensor as T
-import numpy as np
-from theano.ifelse import ifelse
-from random import random
